4037d26bb1e236c793d1eb22a38473451b059054
Allen Kwok <akwok@xerafloptech.com>
2016-06-02 22:39:58 +0000

    Added Province model that is associated to Customer and has foreign key constraints

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
+  belongs_to :province #indicates that a customer can belong to a single province
   validates :full_name, presence: true
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,4 @@
 class Province < ActiveRecord::Base
+  has_many :customers #indicates that a single province would have many customers
+  validates :name, presence: true
 end
