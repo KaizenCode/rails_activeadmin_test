82ebfa2196336d05084f7d1bfe1ff354c9b33170
Allen Kwok <akwok@xerafloptech.com>
2016-06-02 21:29:58 +0000

    Wired up root page which accesses finder#index

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,2 +1,8 @@
 class FinderController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+  # The @customers variable will now be accessible from
+  # the automatically loaded view: app/views/finder/index.html.erb
+
 end
