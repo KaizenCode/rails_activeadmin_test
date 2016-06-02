1eee25a7155d8c4657a5a94f1c3c38eb1093caa6
Allen Kwok <akwok@xerafloptech.com>
2016-06-02 22:10:21 +0000

    Modified so that all controller actions point to index

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -3,13 +3,19 @@ class FinderController < ApplicationController
     # The @customers variable will now be accessible from
     # the automatically loaded view: app/views/finder/index.html.erb
     @customers = Customer.all
+    @page_title = "All Customers"
+    # render :index #this is unnecessary
   end
 
   def alphabetized
     @customers = Customer.order(:full_name)
+    @page_title = "Alphabetized Customers"
+    render :index #will force render the index view instead of alphabetized
   end
 
   def missing_email
     @customers = Customer.where(email_address: '')
+    @page_title = "Customers missing email"
+    render :index
   end
 end
