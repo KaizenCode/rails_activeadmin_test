7ff68cbd35fdf3f854c854fc923c7eaf9b1f74c9
Allen Kwok <akwok@xerafloptech.com>
2016-06-03 22:15:12 +0000

    Playing with active admin

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,6 +1,26 @@
 ActiveAdmin.register Customer do
+  menu parent: "Customer Info"
 
   permit_params :full_name, :phone_number, :email_address, :notes, :province_id
+
+  show do
+    h3 "Hi there"
+    div class: "hi", onclick: "alert('test')", randomtag: "magic" do
+      "This is a test"
+    end
+
+    columns do
+      column do
+        span "Column 1"
+      end
+      column do
+        span "Column 2"
+      end
+    end
+  end
+  #
+  # menu false
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
