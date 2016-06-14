e54fc977d4ea3c2e9521e8dbc8f0092f4f6363db
Allen Kwok <akwok@xerafloptech.com>
2016-06-14 13:57:22 -0700

    Added a sub resource page and linked it to a separate view file

--- app/admin/artist.rb
+++ app/admin/artist.rb
@@ -4,6 +4,10 @@ ActiveAdmin.register Artist do
 
   permit_params :name
 
+  member_action :sync, method: :get do
+    #Establishes the route for sync
+  end
+
   def display_search
     show do
       url = "https://api.spotify.com/v1/search?type=artist&q=#{artist.name}"
@@ -17,6 +21,22 @@ ActiveAdmin.register Artist do
 
   display_search
 
+  controller do
+    # para "Sync settings!"
+
+    def sync
+      @artist = Artist.find(params[:id])
+      # @showsync = true
+
+      # if render not specififed, it will look for the default view in /views/admin/artists/sync
+
+      # render :show #renders the default show
+      # render 'admin/artists/sync1.html.erb', :layout => false #renders a completely separate view
+    end
+  end
+  # def showsync
+  #   para "Sync settings!"
+  # end
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
